47b5d05888ce8d4ef861e3902d698cde6b8969d0
amirhosf <amirhosf@usc.edu>
2019-04-03 19:02:10 -0700

    moved files

--- Main.py
+++ Main.py
@@ -4,4 +4,21 @@ Created on Wed Apr  3 14:03:16 2019
 
 @author: Amirhossein Forouzani
 """
-
+import sklearn
+from sklearn.model_selection import StratifiedKFold
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression
+import csv
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from sklearn.datasets import load_digits
+from sklearn.linear_model import Perceptron
+from IPython.core.interactiveshell import InteractiveShell
+from sklearn.svm import SVC
+#-------------------------------------------------------------------------
---
+++ Preprocessing.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 14:41:43 2019
+
+@author: Amirhossein Forouzani
+"""
+
